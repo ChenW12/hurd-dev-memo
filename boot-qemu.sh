ceedc48590c07d87b5b5cb1359f705298d8a3e7b
ChenW12 <zhmingluo@163.com>
2025-04-10 16:55:27 +0800

    Typo fixed

--- boot-qemu.sh
+++ boot-qemu.sh
@@ -1,3 +1,3 @@
 qemu-system-i386 -m 4G -enable-kvm -drive cache=writeback,file=hurd.img \
-			-net user,hostfwd=tcp:127.0.0.1:2222-:22
+			-net user,hostfwd=tcp:127.0.0.1:2222-:22 \
 			-net nic,model=e1000
